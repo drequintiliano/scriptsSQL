f2e0853acc9769b378076e3933d37af452f3396e
André Quintiliano Filho <drequintiliano@gmail.com>
2017-06-08 13:55:46 -0300

    Create insertTableExe17.sql

---
+++ insertTableExe17.sql
@@ -0,0 +1,15 @@
+# scriptsSQL
+SQL studys
+
+insert into exclientes values ('90001','Donato Campos','R. Sergipe, 27','323-5594','9797979','393393939393','M');
+insert into exclientes values ('90037','Arlindo Garcia','Av Arariboia, 27','428-5594','434343434','1231231231234','M');
+insert into exclientes values ('90024','Felipe Castro','R. Holanda, 27','323-8888','787878787','14141414141','M');
+insert into exclientes values ('90098','Maria de Souza','R. Bandeira, 27','323-7714','5050505','43434343434','F');
+insert into exclientes values ('90063','José Torres','R. Acacias, 27','323-2222','101010101','767676767676','M');
+
+insert into movantigos values (100001,'1018','06/07/1994','C',25,'Deposito',25);
+insert into movantigos values (100002,'1023','06/07/2013','C',100,'Deposito',100);
+insert into movantigos values (100003,'1018','06/07/2016','D',10,'Cheque',15);
+insert into movantigos values (100004,'1023','06/07/2000','C',150,'Deposito',250);
+insert into movantigos values (100005,'1022','06/07/2004','C',50,'Deposito',50);
+insert into movantigos values (100006,'1021','06/07/1994','C',20,'Cheque',20);
