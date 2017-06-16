4a3c882ef9a0660b0b1bd9ccb451d421cdb50402
André Quintiliano Filho <drequintiliano@gmail.com>
2017-06-16 14:24:50 -0300

    Update insertTableExe17.sql

--- insertTableExe17.sql
+++ insertTableExe17.sql
@@ -1,15 +1,25 @@
 # scriptsSQL
 SQL studys
 
-insert into exclientes values ('90001','Donato Campos','R. Sergipe, 27','323-5594','9797979','393393939393','M');
-insert into exclientes values ('90037','Arlindo Garcia','Av Arariboia, 27','428-5594','434343434','1231231231234','M');
-insert into exclientes values ('90024','Felipe Castro','R. Holanda, 27','323-8888','787878787','14141414141','M');
-insert into exclientes values ('90098','Maria de Souza','R. Bandeira, 27','323-7714','5050505','43434343434','F');
-insert into exclientes values ('90063','José Torres','R. Acacias, 27','323-2222','101010101','767676767676','M');
 
-insert into movantigos values (100001,'1018','06/07/1994','C',25,'Deposito',25);
-insert into movantigos values (100002,'1023','06/07/2013','C',100,'Deposito',100);
-insert into movantigos values (100003,'1018','06/07/2016','D',10,'Cheque',15);
-insert into movantigos values (100004,'1023','06/07/2000','C',150,'Deposito',250);
-insert into movantigos values (100005,'1022','06/07/2004','C',50,'Deposito',50);
-insert into movantigos values (100006,'1021','06/07/1994','C',20,'Cheque',20);
+insert into cliente1 values (10010,'Alfredo Ramas',615,'844-2573',0);
+insert into cliente1 values (10011,'Leona Dune',713,'844-2572',0);
+insert into cliente1 values (10012,'Kathy Smith',615,'844-2571',345.86);
+insert into cliente1 values (10013,'Paul Olowski',615,'844-2570',536.75);
+insert into cliente1 values (10014,'Myron Orlando',615,'844-2574',0);
+insert into cliente1 values (10015,'Amy Brian',713,'844-2575',0);
+insert into cliente1 values (10016,'James Brown',615,'844-2576',221.19);
+insert into cliente1 values (10017,'George Willians',615,'844-2577',768.93);
+insert into cliente1 values (10018,'Anne Farris',713,'844-2578',216.55);
+insert into cliente1 values (10019,'Olete Smith',615,'844-2579',0);
+
+insert into cliente_antigo values (10020,'Eduardo Ramas',615,'844-2573',0);
+insert into cliente_antigo values (10021,'Mary Dune',713,'844-2572',0);
+insert into cliente_antigo values (10022,'Marina Smith',615,'844-2571',345.86);
+insert into cliente_antigo values (10023,'Paul da Silva Sauro',615,'844-2570',536.75);
+insert into cliente_antigo values (10024,'Orlando Reis',615,'844-2574',0);
+insert into cliente_antigo values (10025,'Brian da Silva',713,'844-2575',0);
+insert into cliente_antigo values (10026,'James Bond',615,'844-2576',221.19);
+insert into cliente_antigo values (10027,'George Willians',615,'844-2577',768.93);
+insert into cliente_antigo values (10028,'Anne Farris',713,'844-2578',216.55);
+insert into cliente_antigo values (10029,'Olete Smith',615,'844-2579',0);
